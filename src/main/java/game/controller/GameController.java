66b7701f03b3b70deecedf9aa886809ad96cec33
AhmedKhaledS <ahmedkhaled1123300@gmail.com>
2018-11-22 19:11:28 +0200

    Added GameController class.

---
+++ src/main/java/game/controller/GameController.java
@@ -0,0 +1,20 @@
+package game.controller;
+
+import game.CurrentPlayer;
+
+public class GameController {
+
+	/**
+	 * Returns the winning player.
+	 */
+//	public CurrentPlayer run(Agent firstAgent, Agent secondAgent) {
+//		while (true) {
+//			
+//		}
+//	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
