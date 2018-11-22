2c62d4e34830c40976b8fb39a5cfea97aeda41c7
H2O96 <hisho_96@yahoo.com>
2018-11-23 00:06:37 +0200

    Adding more implementation for the aggressive agent

--- src/main/java/agents/AggressiveAgent.java
+++ src/main/java/agents/AggressiveAgent.java
@@ -2,7 +2,10 @@ package agents;
 
 import java.util.ArrayList;
 
+import game.Player;
+import game.state.Attack;
 import game.state.GameState;
+import game.world.Continent;
 import game.world.Country;
 
 public class AggressiveAgent implements Agent {
@@ -14,7 +17,7 @@ public class AggressiveAgent implements Agent {
 	}
 
 	void place(GameState state) {
-		
+
 		// Get the owned countries from the "GameState" class
 		ArrayList<Country> countries = state.getOwnedCountries();
 
@@ -29,17 +32,67 @@ public class AggressiveAgent implements Agent {
 				maxCountryValue = currValue;
 			}
 		}
-		
+
 		System.out.println("Country #" + maxCountryIndex + "is chosen");
 
 	}
 
-	void attack() {
+	void attack(GameState state) {
 
+		ArrayList<Attack> attacks = state.getLegalCountriesAttack();
+
+		Continent maxContinent = getMaxContinent(state, attacks);
+
+		int playerCountryIndex = -1;
+		int opponentCountryIndex = -1;
+		int maxDamage = Integer.MAX_VALUE;
+
+		for (Attack attack : attacks) {
+			Country playerCountry = attack.getAttackingCountry();
+			Country opponentCountry = attack.getAttackedCountry();
+			int currDamage = playerCountry.getArmiesSize() - opponentCountry.getArmiesSize();
+			if (currDamage > maxDamage) {
+
+				if (maxContinent != null &&
+						!maxContinent.equals(opponentCountry.getContinent())) {
+					continue;
+				}
+
+				playerCountryIndex = playerCountry.getIndex();
+				opponentCountryIndex = opponentCountry.getIndex();
+				maxDamage = currDamage;
+			}
+		}
+
+		System.out.println("Country" + playerCountryIndex + "is attacking Country" + opponentCountryIndex);
 	}
 
 	void transfer() {
 
+	}
+
+	private Continent getMaxContinent(GameState state, ArrayList<Attack> attacks) {
+
+		Player currentPlayer = state.getPlayerTurn(), opponentPlayer;
+
+		if (currentPlayer == Player.PLAYER_1) {
+			opponentPlayer = Player.PLAYER_2;
+		} else {
+			opponentPlayer = Player.PLAYER_1;
+		}
+
+		Continent maxContinent = null;
+		int maxContinentValue = Integer.MIN_VALUE;
+
+		for (Attack attack : attacks) {
+			Continent continent = attack.getAttackedCountry().getContinent();
+			if (continent.getContinentOwner() == opponentPlayer && continent.getContinentBonus() > maxContinentValue) {
+				maxContinent = continent;
+				maxContinentValue = continent.getContinentBonus();
+			}
+		}
+
+		return maxContinent;
 	}
 
 }
