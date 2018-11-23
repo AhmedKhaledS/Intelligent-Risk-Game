68cd5f8dfb184d08f3ccc95142d3051d1307ff27
ahmednaggar19 <ahmednagar2011@gmail.com>
2018-11-23 21:52:30 +0200

    Implemented A* Algorithm

--- src/main/java/agents/intelligent/AStarAgent.java
+++ src/main/java/agents/intelligent/AStarAgent.java
@@ -1,7 +1,10 @@
 package agents.intelligent;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Set;
 
@@ -26,27 +29,60 @@ public class AStarAgent extends InformedSearchAgent {
 		Set<GameState> visited = new HashSet<>();
 		boolean firstTurn = true;
 		BounsCalculator bonusCalculator = BounsCalculator.getInstance();
+		Map<GameState, Attack> attacks = new HashMap<>();
 		while (!searchQueue.isEmpty()) {
 			GameState current = searchQueue.poll();
+			if (current.isTerminal()) {							// Terminal State : backtracking to get the list of attacks done in the path to terminal state.
+				while (current.getPreviousState() != null) {
+					moves.add(attacks.get(current));
+					current = current.getPreviousState();
+				}
+				Collections.reverse(moves);
+				return;
+			}
 			visited.add(current);
-			if (!firstTurn) {
+			if (!firstTurn) {  									//	Opponent Agent does not take turn in first iteration. (Player 1 starts)		
 				opponentAgent.takeTurn(current);
 			}
+			firstTurn = false;
 			int bonusArmies = bonusCalculator.getBonus(current, current.isPrevTurnAttacked());
 			ArrayList<Country> ownedCountries = current.getOwnedCountries();
-			for (Country country : ownedCountries) {
+			for (Country country : ownedCountries) {			// Loop over all possible placement of armies (Over owned countries).
 				ArmyPlacement placement = new ArmyPlacement(country, bonusArmies);
 				GameState virtual = current.forecastArmyPlacement(placement);
-				ArrayList<Attack> possibleAttacks = virtual.getLegalCountriesAttack(); /// TODO : current -> virtual
-				/// TODO : Clone GameStates on the size of legalAttacks.
-				if (possibleAttacks.isEmpty()) {
-					Attack nonAttack = new Attack(null, null);
-					
+				ArrayList<Attack> possibleAttacks = virtual.getLegalCountriesAttack();
+				Attack nonAttack = new Attack(null, null);		// Add the state reached when no attack is made.
+				virtual.setCost(heuristic.getHeuristicValue(virtual) + virtual.getDepth());
+				virtual.setPrevTurnAttacked(false);
+				addGameState(searchQueue, visited, virtual);
+				attacks.put(virtual, nonAttack);
+				if (!possibleAttacks.isEmpty()) {				// Handle possible attacks and check for each one of them the possible transfer of armies.
+					for (Attack possibleAttack : possibleAttacks) {
+						Country attacking = possibleAttack.getAttackingCountry();
+						int armies = attacking.getArmiesSize() - possibleAttack.getAttackedCountry().getArmiesSize();
+						for (int onAttacking = 1; onAttacking < armies; onAttacking++) {		// Loop over all possible transfer of armies from attacking to attacked.
+							int onAttacked = armies - onAttacking;
+							Attack newAttack = possibleAttack.clone();
+							newAttack.setArmyTransferCount(onAttacked);
+							GameState newVirtual = virtual.clone();
+							newVirtual.setPrevTurnAttacked(true);
+							newVirtual.applyAttack(newAttack);
+							newVirtual.setCost(heuristic.getHeuristicValue(newVirtual) + newVirtual.getDepth());
+							addGameState(searchQueue, visited, newVirtual);
+							attacks.put(newVirtual, newAttack);
+						}
+					}
 				}
 			}
 			
 		}
 	}
 
+	public void addGameState(PriorityQueue<GameState> searchQueue, Set<GameState> visited, GameState state) {
+		if (!visited.contains(state)) {
+			searchQueue.add(state);
+		}
+		return;
+	}
 	
 }
